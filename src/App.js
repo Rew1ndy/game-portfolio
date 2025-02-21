f455ba796aa20cb3cf65ae79d3f1fd80a32826c8
AlexMega <matvienko.alex1205@gmail.com>
2025-02-21 20:59:16 +0200

    revert

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Home"; 
-import Gallery from "./Gallery"; 
-import About from "./About"; 
-import News from "./News"; 
-import Footer from "./Footer"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import News from "./pages/News";
+import Footer from "./components/Footer";
+import "./App.css";
 
-const App = () => {
+function App() {
   return (
-    <Router basename="/game-portfolio">
+    <Router>
       <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/news" element={<News />} />
-        </Routes>
+        <div className="lines">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
 
+        <Navbar />
+
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
+ 
         <Footer />
       </div>
     </Router>
   );
-};
+}
 
 export default App;
